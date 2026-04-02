d36f198f92ea8b75a76fc1908616e1f2391b5c88
ktj <xowls0304@gmail.com>
2026-04-02 12:33:05 +0900

    20260402

--- src/chapter/chapter01_Data/C010_Char.java
+++ src/chapter/chapter01_Data/C010_Char.java
@@ -31,5 +31,34 @@ public class C010_Char {
 		c = (char) (c+1);
 		System.out.println(c);
 		
+		/*
+		[탈출 문자(Escape Sequence)]
+		-문자열 안에서 특수 기능을 수행하는 문자
+		\n : 줄바꿈
+		\t : 탭
+		\\ : \ 출력
+		\' : ' 출력
+		\" : " 출력
+		 */
+		
+		System.out.println("안\n녕\t하\\세\'요\"");
+		System.out.println("\"안녕\"하세요");
+		
+		/*
+	[문자열(String)]
+	-문자들의 집합(char 여러개 모인것)
+	-String은 참조형 데이터 타입 -> 힙에 저장
+	 */
+		
+		String str = "대한민국";
+		System.out.println(str);
+		
+		//탈출 문자를 사용한 문자열
+		String str2 = "아름다운 이 땅에 금수강산에 "
+					+"단군 할아버지가 \"터\" 잡으시고\n"
+					+"\'홍익인간\' 뜻으로 나라 세우니 "
+					+"대대손손 휼륭한 인물도 많아.";
+		System.out.println(str2);
 	}
+	
 }
---
+++ src/chapter/chapter01_Data/C011_TextBlock.java
@@ -0,0 +1,48 @@
+package chapter.chapter01_Data;
+
+public class C011_TextBlock {
+	
+	public static void main(String[] args) {
+	
+		/*
+		[텍스트 블록(Text Block)]
+		java 15 ver 이상
+		"""로 여러 줄 문자열 작성할 수 있음
+		 
+		 특징
+		 1. 여러줄 문자열 쉽게 작성
+		 2.  자동 줄바꿈 처리
+		 3. 따옴표 escape 필요없음
+		 4. html / json / sql 작성 매우 편리
+		 */
+		
+		// 텍스트 블록 방식
+		String textBlock = """
+				이름:"홍길동"
+				나이:20세
+				키:189.9cm
+				직업: 	'프로그래머'
+				""";
+		System.out.println(textBlock);
+		
+		// sql 작성 예시
+		String sql = """
+				SELECT *
+				FROM users
+				WHERE age > 20
+				ORDER BY name
+				""";
+		System.out.println(sql);
+		
+		// formatted()
+		// 문자열 포맷 가능
+		String name = "홍길동";
+		int age = 30;
+		String message = """
+				이름:%s
+				나이:%d
+				""".formatted(name,age);
+		System.out.println(message);
+	
+	}
+}
