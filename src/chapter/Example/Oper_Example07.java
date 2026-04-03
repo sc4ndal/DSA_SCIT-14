e625508efac678f0f53910c5f260c9e78536bf65
HaruAki <100898033+sc4ndal@users.noreply.github.com>
2026-04-03 18:26:47 +0900

    Oper_Example07.java 업데이트
    20260403

--- src/chapter/Example/Oper_Example07.java
+++ src/chapter/Example/Oper_Example07.java
@@ -6,8 +6,10 @@ public class Oper_Example07 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
-		System.out.println("정수를 입력하세요 : ");
-		int a = sc.nextInt();
-		String result = a/2=2?"짝수"
+        System.out.print("정수 입력: ");
+        int a = sc.nextInt();
+		
+		String result = (a%2==0)? "짝수" : "홀수";
+		System.out.println("결과: "+result);
 	}
 }
