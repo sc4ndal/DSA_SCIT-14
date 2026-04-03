650c0bd00509df94b2e58f577184bba79ea8b5df
ktj <xowls0304@gmail.com>
2026-04-03 17:55:03 +0900

    20260402

---
+++ src/chapter/Example/Oper_Example07.java
@@ -0,0 +1,13 @@
+package chapter.Example;
+
+import java.util.Scanner;
+
+public class Oper_Example07 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("정수를 입력하세요 : ");
+		int a = sc.nextInt();
+		String result = a/2=2?"짝수"
+	}
+}
