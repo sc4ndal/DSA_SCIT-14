c958d7c4a68f87b67e892f46f76d5c8520354fc4
ktj <xowls0304@gmail.com>
2026-04-06 09:00:49 +0900

    20260402

--- src/chapter/Example/Oper_Example06.java
+++ src/chapter/Example/Oper_Example06.java
@@ -5,17 +5,16 @@ import java.util.Scanner;
 public class Oper_Example06 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int h,m,s,sec;
+		int h, m, s, sec;
 		
-		System.out.println("초를 입력하세요: ");
+		System.out.print("초를 입력하세요: ");
 		sec = sc.nextInt();
 		h = (sec / 3600);
 //		System.out.println(h);
-		m = (sec / 60)-(h*60);
+		m = (sec / 60) - (h * 60);
 //		System.out.println(m);
-		s=sec%60;
+		s = sec % 60;
 //		System.out.println(s);
-		System.out.println("초를 입력하세요: ");
 		System.out.println(h+"시간 "+m+"분 "+s+"초");
 		
 		
