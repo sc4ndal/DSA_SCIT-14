f4d779c5341d0410aaba78a434f8e429acac2ada
ktj <xowls0304@gmail.com>
2026-04-06 12:29:53 +0900

    20260406

--- src/practice/ex.java
+++ src/practice/ex.java
@@ -3,12 +3,14 @@ package practice;
 public class ex {
 	public static void main(String[] args) {
 		
-		int i=0, sum = 0;
+		int i = 0, sum = 0;
+		
 		for (i = 0; i<=10; i++){
-			if(i%2==1){
+			if (i % 2 == 1) {
+				
 				continue;
 			}
-			sum +=i;
+			sum += i;
 		}
 		System.out.println(sum);
 
